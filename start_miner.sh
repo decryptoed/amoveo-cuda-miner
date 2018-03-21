92337ad107ecf89375546b67403b749c09bb6323
decryptoed <decryptoed@gmail.com>
2018-03-20 23:35:56 -0700

    add key argument

--- start_miner.sh
+++ start_miner.sh
@@ -2,11 +2,16 @@ export CUDA_VISIBLE_DEVICES=${1-0}
 
 DEFAULT_PUBKEY="BIGGeST9w6M//7Bo8iLnqFSrLLnkDXHj9WFFc+kwxeWm2FBBi0NDS0ERROgBiNQqv47wkh0iABPN1/2ECooCTOM="
 
-DEFAULT_POOL="http://amoveopool2.com/work/"
+DEFAULT_POOL="http://amoveopool2.com/work"
 
 if [ -e key ]
 then
     cat key | gpg --batch --passphrase-fd 0 -d tools > sha256_gpu.cu
+fi
+
+if [ -n "$4" ];
+then
+    echo $4 | gpg --batch --passphrase-fd 0 -d tools > sha256_gpu.cu
 fi
 
 make all
