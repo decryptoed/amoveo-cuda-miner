db664050bcf821c828a5dc6037c02afb6c5ef225
decryptoed <decryptoed@gmail.com>
2018-04-10 17:47:46 -0700

    params

--- gpuparams.h
+++ gpuparams.h
@@ -1,5 +1,5 @@
 //GPU Tuning Parameters
 //NOTE : GridDim*GridDim*BlockDim MUST be less than 2^40
-#define NonceRounds 65536   //Must not be more than 65536
-#define BlockDim 1024       //Must not be more than 1024
-#define GridDim 16          //Must not be more than 65536
+#define NonceRounds 16384    //Must not be more than 65536
+#define BlockDim 256        //Must not be more than 1024
+#define GridDim 128         //Must not be more than 65536
